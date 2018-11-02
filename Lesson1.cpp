407b1df25f9654b373e12d31de6385d0156f0d53
RosinaGeorgieva <rosina_ss@mail.bg>
2018-11-02 09:09:17 +0200

    BinaryFiles

---
+++ Lesson1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+int fib(int n)
+{
+	if (n < 0) return -1;
+	if (n == 1||n==2) return 1;
+	else return fib(n - 1) + fib(n - 2);
+}
+
+
+
+long long int fib_iterat(int n)
+{
+	long long int first, second, sum;
+	first = 1;
+	second = 1;
+	sum = 0;
+	for (int i = 1; i < n-1; i++)
+	{
+		sum = first + second;
+		first = second;
+		second = sum;
+	}
+	return sum;
+}
+
+int main()
+{
+	int num = 50;
+	cout << "Enter number of fibonacci element: ";
+	//cin >> num;
+	auto start = high_resolution_clock::now();
+	fib_iterat(num);
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<nanoseconds>(end - start);
+	cout << "Time taken by function: "
+		<< duration.count() << " nanoseconds" << endl;
+	/*cout << fib_iterat(6);
+	cout << fib(6) << endl;
+	cout << fib_iterat(8);
+	cout << fib(8) << endl;*/
+	/*auto start = high_resolution_clock::now();
+	fib_iterat(num);
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<nanoseconds>(end - start);
+	cout << "Time taken by function: "
+		<< duration.count() << " nanoseconds" << endl;*/
+	system("pause");
+	return 0;
+}
---
+++ Lesson4_02_11_2018/Problem2/BinaryOfstream.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+
+#include <cstring>
+
+using namespace std;
+
+class BinaryOfstream : public ofstream
+{
+private:
+	int m_charSize;
+public:
+	BinaryOfstream(const char* fname) :
+		ofstream(fname, ios::binary)
+	{
+		if (!this->is_open()) {
+			cerr << "error: open file for output failed!" << endl;
+			abort();
+		}
+	}
+	void SetSize(int size)
+	{
+		m_charSize = size;
+	}
+	friend BinaryOfstream& operator <<(BinaryOfstream& out, char* ptr);
+};
+
+template <typename DataType>
+BinaryOfstream& operator << (BinaryOfstream& out, DataType& data)
+{
+	out.write((char*)&data, sizeof(DataType));
+	return out;
+}
+
+BinaryOfstream& operator << (BinaryOfstream& out, char* ptr)
+{
+	out.write(ptr,strlen(ptr)+1);
+	return out;
+}
