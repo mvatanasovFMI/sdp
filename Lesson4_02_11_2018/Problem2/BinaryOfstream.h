7c7a9897b8ad95cded1bb515d2a8dfdfd1a9905d
ILIAN\Ilian1 <ilian2105@abv.bg>
2018-11-09 07:57:40 +0200

    Changes from last week.

---
+++ Lesson4_02_11_2018/Problem2/BinaryOfstream.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma once
+#include <fstream>
+#include <iostream>
+
+#include <cstring>
+
+using namespace std;
+
+class BinaryOfstream : public ofstream
+{
+private:
+	int m_charSize;
+public:
+	BinaryOfstream(const char* fname) :
+		ofstream(fname, ios::binary)
+	{
+		if (!this->is_open()) {
+			cerr << "error: open file for output failed!" << endl;
+			abort();
+		}
+	}
+	void SetSize(int size)
+	{
+		m_charSize = size;
+	}
+	friend BinaryOfstream& operator << (BinaryOfstream& out, char* ptr);
+};
+
+template <typename DataType>
+BinaryOfstream& operator << (BinaryOfstream& out, DataType& data)
+{
+	out.write((char*)&data, sizeof(DataType));
+	return out;
+}
+
+BinaryOfstream& operator << (BinaryOfstream& out, char* ptr)
+{
+	out.write(ptr, out.m_charSize);
+	return out;
+}
