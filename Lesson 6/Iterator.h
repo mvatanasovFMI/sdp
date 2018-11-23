36862aaca6252302fda52b1bb8ed3a6278d494c0
unknown <ninachev@mail.bg>
2018-11-23 08:50:26 +0200

    added Lesson 6

---
+++ Lesson 6/Iterator.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Item.h"
+
+template <class T>
+class Iterator {
+private:
+	Item<T> *m_pStart;
+	Item<T> *m_pCurrent;
+
+public:
+	Iterator(Item<T> *m_pStart) : m_pStart(m_pStart), m_pCurrent(m_pStart)
+	{
+	}
+
+	T const & GetCurrent() const
+	{
+		return m_pCurrent->m_data;
+	}
+
+	void SetCurrent(T const & Value)
+	{
+		m_pCurrent->m_data = Value;
+	}
+
+	void MoveNext()
+	{
+		if (m_pCurrent)
+			m_pCurrent = m_pCurrent->m_pNext;
+	}
+
+	void Rewind()
+	{
+		m_pCurrent = m_pStart;
+	}
+
+	bool EndReached() const
+	{
+		return m_pCurrent == nullptr;
+	}
+};
