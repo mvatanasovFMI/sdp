7f7c037b18dc047ad673ee10c9d02e4fa5de864e
mdobrinov <31880859+mdobrinov@users.noreply.github.com>
2018-11-23 23:16:48 +0200

    23.11 Lesson
    23.11 Lesson

---
+++ iterator.h
@@ -0,0 +1,45 @@
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+#include "item.h"
+
+template <class T>
+class Iterator {
+private:
+	Item<T> *m_pStart;
+	Item<T> *m_pCurrent;
+
+public:
+	Iterator(Item<T> *m_pStart) : m_pStart(m_pStart), m_pCurrent(m_pStart)
+	{
+	}
+
+	T const & GetCurrent() const
+	{
+		return m_pCurrent->m_data;
+	}
+
+	void SetCurrent(T const & Value)
+	{
+		m_pCurrent->m_data = Value;
+	}
+
+	void MoveNext()
+	{
+		if (m_pCurrent)
+			m_pCurrent = m_pCurrent->m_pNext;
+	}
+
+	void Rewind()
+	{
+		m_pCurrent = m_pStart;
+	}
+
+	bool EndReached() const
+	{
+		return m_pCurrent == nullptr;
+	}
+};
+
+#endif // !_ITERATOR_H_
+
